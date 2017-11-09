236eacaddc8f2ca5463b58f16dbed26818a98c3b
Student <Student@K47-13>
2017-11-09 19:20:59 +0200

    Admin

---
+++ Magazine/Magazine/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary1.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Magazine.Controllers
+{
+    public class AdminController : Controller
+    {
+        Model1 context;
+     public AdminController()
+        {
+            context = new Model1();
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var model = context.GoodsDbSet;
+            return View(model);
+        }
+
+        public ActionResult Edit()
+        {
+            var model = context.GoodsDbSet;
+            return View(model);
+        }
+
+    }
+}
